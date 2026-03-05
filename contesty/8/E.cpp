97e9a2301105d29062e9693f60f96bec9e77e523
Chirockat <babislukaszstudy@gmail.com>
2026-03-05 15:49:48 +0100

    Update E.cpp

--- contesty/8/E.cpp
+++ contesty/8/E.cpp
@@ -2,16 +2,17 @@
 #include<iostream>
 #include<vector>
 #include<numeric> 
+#include<cstdint>
 
 using namespace std;
 
-
+#define print(var) cout << #var <<": " << var << endl;
 
 
 int main() {
 
-    int32_t n, t;
-    cin >> n >> t;
+    int32_t n, p;
+    cin >> n >> p;
 
     int32_t arr [n];
 
@@ -20,6 +21,7 @@ int main() {
         cin >> i;
     }
 
+<<<<<<< HEAD
     int64_t a = 1;
     int64_t b = n * t;
 
@@ -72,11 +74,65 @@ int main() {
         else {
             b = b/2;
             local_best = sum_b;
+=======
+    int64_t max = INT64_MAX;
+
+
+
+    // iloraz, poprzedni iloraz
+    int64_t quotient = max;
+    int64_t prev_quotient;
+
+    int64_t local_sum;
+
+    // szukamy, idąc od góry zakresy, największego dla ktorego nie uda sie wyrobic targetu
+    do {
+        local_sum = 0;
+        prev_quotient = quotient;
+        quotient = quotient/2;
+
+        for (int i = 0; i < n; i++) {
+            local_sum += quotient / arr[i];
+            if (local_sum >= p) {
+                break;
+            }
+        }
+    } while (local_sum >= p);
+
+    int64_t middle;
+
+    //przerabiamy nazwy zmniennych tak na prawde, bo teraz maja wiekszy sens a i b
+    int64_t a = quotient;
+    int64_t b = prev_quotient;
+
+    //wchodzimy z zakresem [a,b] i go zmniejszamy ciagle
+    while (b - a > 1) {
+        local_sum = 0;
+
+        middle = (a + b) / 2;
+
+        for (int i = 0; i < n; i++) {
+            local_sum += middle / arr[i];
+            if (local_sum >= p) {
+                break;
+            }
+>>>>>>> ce4b854fdd2e3e0d2b23159b5c168581c000d64e
         }
 
-        if (best_sum - t > local_best - t) {
-            best_sum = local_best;
+        if (local_sum >= p) {
+            b = middle;
         }
         else {
+<<<<<<< HEAD
             break;
         }
+=======
+            a = middle;
+        }
+    }
+
+    // wynik przy naszej opcji bedzie w b (troche to zagmatwane...)
+    cout << b;
+
+}
+>>>>>>> ce4b854fdd2e3e0d2b23159b5c168581c000d64e
